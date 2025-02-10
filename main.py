67219ed2dfa3e712f321bc82127170f634e0a3f3
env-gaurav <gaurav2528singh@gmail.com>
2025-02-10 23:07:57 +0530

    practice questions

--- main.py
+++ main.py
@@ -31,3 +31,48 @@ nums={}
 for i in range(1,11):
     nums[i]=i**2
 print(nums)
+
+'''
+Write a Python script to add a key to a dictionary.
+
+Sample Dictionary : {0: 10, 1: 20}
+Expected Result : {0: 10, 1: 20, 2: 30}
+'''
+def keytoDict(n):
+    data = {}
+    base_value = 10
+    for i in range(n+1):
+        data[i] = base_value * i + base_value
+    print(data)
+keytoDict(5)
+
+'''
+Write a Python script to concatenate the following dictionaries to create a new one.
+
+Sample Dictionary :
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+'''
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+for i in (dic1,dic2,dic3):
+    dic1.update(i)
+print(dic1)
+
+'''
+Write a Python script to check whether a given key already exists in a dictionary.
+'''
+employee= {"emp_id":101, "emp_name":"Rahul", "emp_dept":"HR"}
+print(employee.get("emp_id","not exist"))
+print(employee.get("emp_salary","not exist"))
+
+'''
+Write a Python program to iterate over dictionaries using for loops.
+'''
+flight= {"IATA":["AI","6E","QP"], "airline":["Air_India","Indigo","Akasa_air"]}
+length= len(flight["IATA"])
+for i in range(length):
+    print(flight["IATA"][i],">-" , flight["airline"][i])
